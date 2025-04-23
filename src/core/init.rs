c1f906c7803b3852ff228457f3d08850df1d1193
Gemiman <913573586@qq.com>
2025-04-23 15:36:00 +0800

    更新版本号至 0.2.0，添加初始化模块并配置 tracing_subscriber

--- src/core/init.rs
+++
@@ -1,6 +0,0 @@
-use crate::constants::APP_NAME;
-
-pub fn init() {
-    tracing_subscriber::fmt().init();
-    tracing::info!("{} init...finish...", APP_NAME);
-}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,8 @@
-pub mod init;
-
 pub use tracing;
 pub use tracing_subscriber;
+use crate::constants::APP_NAME;
+
+pub fn init() {
+    tracing_subscriber::fmt().init();
+    tracing::info!("{} init...finish...", APP_NAME);
+}
