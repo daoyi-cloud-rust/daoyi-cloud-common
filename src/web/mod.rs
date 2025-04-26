f88f195369566bd0a146a8aa9ed488e0c3767432
Gemiman <913573586@qq.com>
2025-04-26 12:24:50 +0800

    更新版本号至 0.2.7，添加多个新依赖项以增强功能

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,3 +1,7 @@
 pub use salvo::prelude::*;
 pub use tokio;
 pub use crate::core::*;
+pub use salvo::catcher::Catcher;
+pub use salvo::conn::rustls::{Keycert, RustlsConfig};
+pub use salvo::server::ServerHandle;
+pub use serde::Serialize;
